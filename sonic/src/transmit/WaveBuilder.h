465dedc768077c6c75fd2d05aeae148d57b4c760
吴敏 <wumin@tuya.com>
2018-06-25 11:39:39 +0800

    update

--- sonic/src/transmit/WaveBuilder.h
+++
@@ -1,9 +0,0 @@
-#ifndef __WAVEBUILDER_H__
-#define __WAVEBUILDER_H__
-
-int getSampleCount(int sampleRate, int sampleChannels, double duration);
-
-int makeWave(int sampleRate, int sampleChannels, double duration,
-				int freqValues[], int freqCount, short* out);
-
-#endif
